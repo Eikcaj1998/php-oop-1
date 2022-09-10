f7a91bffb644b1131cfcfc2df1a7e4c26a4a7055
Eikcaj.1998 <jw220998@gmail.com>
2022-09-10 17:19:16 +0200

    esercizo finito

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php
-    include _DIR_ . 'class/movie.php';
+   class Movie 
+   {
+    public $id;
+    public $title;
+    public $sub_title;
+    public $genre;
+    public $author;
+    public function  __construct($id, $title, $sub_title, $genre, $author)
+    {
+        $this->id = $id;
+        $this->title = $title;
+        $this->sub_title = $sub_title;
+        $this->genre = $genre;
+        $this->author = $author;
+    }
+   }
+   $films = [];
+   $films[]= new Movie('1', 'Pirati Dei Caraibi', 'ai confini del mare','azione,avventura','Rob Kidd');
+   $films[] = new Movie('2', 'Pirati Dei Caraibi', 'la maledizione di Salazar','azione,avventura','Rob Kidd');
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,6 +29,12 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <ul>
+        <?php foreach ($films as $film) { ?>
+            <li>
+                <?php echo $film->title . " - " . $film->sub_title . " - " . $film->genre . " - " . $film->author ?>
+            </li>
+        <?php } ?>
+    </ul>
 </body>
 </html>
